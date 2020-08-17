9b1566774a8bcc5d0a88a23ad245382946275f94
Erik Fernando Stor Franco <erik.stor.franco@gmail.com>
2020-08-16 22:22:15 -0500

    Seccion de historia

--- src/partials/Tabs.js
+++ src/partials/Tabs.js
@@ -10,6 +10,7 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
 
 import Projects from './Projects';
 
@@ -19,6 +20,7 @@ css e imagenes
 
 import '../assest/css/main.css'
 import Logo from '../assest/img/logo.png'
+import bannerHistoria_1 from '../assest/img/teamNexura.png'
 
 function TabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -111,7 +113,7 @@ export default function SimpleTabs() {
                                 gutterBottom
                                 className="text-primary"
                             >
-                                Erik Stor
+                                Hola!
                             </Typography>
                             <Typography
                                 variant="h5"
@@ -120,7 +122,7 @@ export default function SimpleTabs() {
                                 paragraph
                                 className="text-primary"
                             >
-                                Hola, bienvenidx a mi peque&ntilde;o espacio en la web.
+                                Mi nombre es Erik Stor, bienvenidx a mi peque&ntilde;o espacio en la web.
                             </Typography>
                             <div
                                 className={classes.heroButtons}
@@ -142,6 +144,18 @@ export default function SimpleTabs() {
                                             Conocem&eacute; un poco!
                                         </Button>
                                     </Grid>
+                                    <Grid item>
+                                        <Button
+                                            variant="contained"
+                                            className="bg-verde-radioactivo"
+                                            onClick={event => {
+                                                ir('simple-tab-2')
+                                            }}
+
+                                        >
+                                            Mis habilidades
+                                        </Button>
+                                    </Grid>
                                 </Grid>
                             </div>
                         </Container>
@@ -150,6 +164,88 @@ export default function SimpleTabs() {
 
             </TabPanel>
             <TabPanel value={value} index={1}>
+
+                <div className="background-historia-main">
+
+                </div>
+
+                <div>
+                    <p>Algo de mi historia…</p>
+                    <p>Nac&iacute; en Dosquebradas, un peque&ntilde;o municipio de Colombia, pa&iacute;s donde
+                        actualmente habito. Es curioso ya que nunca viv&iacute; all&aacute; solamente nac&iacute; y
+                        antes de tener conciencia me mude de a Cali.
+                        Soy desarrollador web fullstack y empec&eacute; en todo ese mundo de la programaci&oacute;n casi
+                        porque la vida me obligo, asi como en la pel&iacute;cula de
+
+                        <Link
+                            target="_blank"
+                            href="https://www.google.com/search?gs_ssp=eJzj4tTP1TdIKskwNTFg9GJLLEssSSwCADYCBbY&q=avatar&oq=avata&aqs=chrome.1.0j46j69i57j46l3j0l2.1991j0j7&sourceid=chrome&ie=UTF-8"
+                        >&nbsp;avatar&nbsp;
+                        </Link>
+
+                        donde no elijes montura, la
+                        montura te elije a ti.</p>
+                </div>
+
+
+                <div className="background-historia-1">
+
+                </div>
+
+                <div>
+                    <p>
+                        Empecé a estudiar en el Sena en el 2016 y debo de decir que mis instructores, más que
+                        simples profesores, se convirtieron en amigos. Y es algo curioso, ya que puedo decir con
+                        orgullo que sin importar con quienes trabaje o estudie, tarde o temprano terminan siendo,
+                        más amigos que mis compañeros y en casos excepcionales mentores.
+                    </p>
+                </div>
+
+                <div className="background-historia-2">
+
+                </div>
+
+                <div>
+                    <p>
+                        Bueno en mis estudios aunque no era el más increíble de mis compañeros, siempre estuve entre los
+                        más destacados y pude ser partícipe de un semillero de investigación SINCO e incluso ser ponente
+                        regional del mismo.
+                    </p>
+                </div>
+
+                <div className="background-historia-3">
+
+                </div>
+
+                <div>
+                    <p>
+                        Bueno y en resumen, luego de terminar mis estudios, trabajé para una de las agencias de
+                        entretenimiento para adultos más grande de mi país (desarrollando páginas web
+                        claramente). Y
+                        por cosas de la vida logre ingresar a una multinacional, donde conocí unas personas
+                        fantásticas en su mayoría y de las cuales pude aprender muchísimo.
+                    </p>
+                    <p>
+                        Actualmente, tengo 2 años desarrollando todo tipo de aplicaciones web y ahora quisiera
+                        compartir lo que he aprendido en algún momento con un par de cursos explicando todo lo que se.
+                    </p>
+                </div>
+
+                <div className="background-historia-3">
+
+                </div>
+
+                <div>
+                    <p>
+
+                        Asi como todo el mundo tengo mis aficiones, y la que más me apasiona a la fecha son las motos.
+                        También me encanta la comida italiana y me encanta aprender constantemente. De hecho actualmente
+                        me encuentro aprendiendo nuevas tecnologías, librerías y frameworks. Creo que es un poco
+                        evidente (por las fotos que he usado en esta página). Pero no está demás decir, que me encanta
+                        la naturaleza. Esta historia se sigue escribiendo y si quieres ser parte de ella puedes
+                        escribirme, en la pestaña de contacto podrás encontrar links directos a mis redes sociales.
+                    </p>
+                </div>
 
             </TabPanel>
             <TabPanel value={value} index={2}>
