d9c795fab3e1335975386d789c355e9a177d1ba0
Erik Fernando Stor Franco <erik.stor.franco@gmail.com>
2020-08-23 13:21:18 -0500

    LinkedIn icon

--- src/components/IconsFooter.js
+++ src/components/IconsFooter.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import {Facebook} from "@material-ui/icons";
+import {LinkedIn} from "@material-ui/icons";
 import Gmail from '../assest/img/gmail.svg';
 import Instagram from '../assest/img/instagram.svg';
+// import LinkedIn from '../assest/img/linkedin.svg';
 import Link from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
 
 const urlFacebook = "https://www.facebook.com/erikgram-2767021989978734";
 const urlInstagram = "https://www.instagram.com/erik.gram/";
+const urlLinkedIn = "https://www.linkedin.com/in/erikstorfranco/";
 const urlGmail = "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSDbSJZtxwNHBBMcZmGdbvcpvHpTMdfWcMdJdJcWQzvccNZzKZtGdtKQLGhmXlddGcsKxjfl";
 
 export default function IconsFooter() {
@@ -15,44 +17,35 @@ export default function IconsFooter() {
     function FormRow() {
         return (
             <React.Fragment>
-                <Grid item xs={4} sm={1} direction="row" justify="center">
+                <Grid item xs={4} sm={1} direction="row" justify="center" className="icon-align-footer-m color-facebook margin-l-2-5">
                     <Link color="inherit"
                           target="_blank"
                           href={urlFacebook}
                     >
-                        <Typography variant="body2"
-                                    color="primary"
-                                    className="icon-align-footer-r"
-                        >
-                            <Facebook/>
-                        </Typography>
+                        <Facebook/>
                     </Link>
-                </Grid>
-                <Grid item xs={4} sm={1} direction="row" justify="center">
                     <Link color="inherit"
                           target="_blank"
                           href={urlInstagram}
+                          className="margin-l-1"
                     >
-                        <Typography variant="body2"
-                                    color="textSecondary"
-                                    className="icon-align-footer-m"
-                        >
-                            <img className="size-gmail-icon" src={Instagram} alt="gmail"/>
-                        </Typography>
+                        <img className="size-instagram-icon" src={Instagram} alt="Instagram"/>
                     </Link>
-
                 </Grid>
-                <Grid item xs={4} sm={1} direction="row" justify="center">
+                <Grid item xs={4} sm={1} direction="row" justify="center" className="color-linkedIn">
+                    <Link color="inherit"
+                          target="_blank"
+                          href={urlLinkedIn}
+                          className="margin-l-negative-1"
+                    >
+                        <LinkedIn/>
+                    </Link>
                     <Link color="inherit"
                           target="_blank"
                           href={urlGmail}
+                          className="margin-l-1"
                     >
-                        <Typography variant="body2"
-                                    color="textSecondary"
-                                    className="icon-align-footer-l"
-                        >
-                            <img className="size-gmail-icon" src={Gmail} alt="gmail"/>
-                        </Typography>
+                        <img className="size-gmail-icon" src={Gmail} alt="Gmail"/>
                     </Link>
                 </Grid>
             </React.Fragment>
--- src/partials/Projects.js
+++ src/partials/Projects.js
@@ -48,56 +48,56 @@ const cards = [
         "nombre": "Gfiles - Solución cloud para gestión documental",
         "descripcion": "GFiles se trataba de un gestor documental “Todo en uno”, en donde una entidad podía gestionar de una forma ultra eficiente todos los documentos que manejase.",
         "dominio": "https://www.gfiles.co/",
-        "image" : gfilesLogo
+        "image": gfilesLogo
 
     },
     {
         "nombre": "Nexura Platform",
         "descripcion": "Nexura Cloud Platform es una solución web personalizable de acuerdo a las necesidades de cada entidad, que ayuda a implementar estrategias de gobierno digital y administrar de forma centralizada múltiples dependencias, secretarías o entidades conexas.",
         "dominio": "https://www.nexura.com/publicaciones/129392/nexura-cloud-platform/",
-        "image" : nxPlatformLogo
+        "image": nxPlatformLogo
 
     },
     {
         "nombre": "WebCam - Boutique",
         "descripcion": "WebCam – Boutique, era una pagina web que funcionaba como tienda virtual, para las modelos de la agencia para la que trabajaba en esos tiempos.",
         "dominio": "https://www.webcamboutique.com/",
-        "image" : webCamBoutiqueLogo
+        "image": webCamBoutiqueLogo
 
     },
     {
         "nombre": "Trend Studios",
         "descripcion": "Este fue uno de mis primeros proyectos, a pesar de ser una página web sencilla, al ser casi la primera que publiqué con éxito, le tengo un cariño especial.",
         "dominio": "https://www.trendstudios.co/",
-        "image" : trendStudiosLogo
+        "image": trendStudiosLogo
 
     },
     {
         "nombre": "Sistema de elección del personero",
         "descripcion": "Durante mis prácticas profesionales, tuve la oportunidad de ingresar a una institución educativa, supremamente reconocida en mi ciudad, para la cual pude desarrollar un sistema de elección del personero",
         "dominio": "https://colegiosarquidiocesanos.edu.co/web/",
-        "image" : SIHLogo
+        "image": SIHLogo
 
     },
     {
         "nombre": "Sistema de inventario de préstamos",
         "descripcion": "Durante mis prácticas profesionales, tuve la oportunidad de ingresar a una institución educativa, supremamente reconocida en mi ciudad, para la cual pude desarrollar un sistema de inventario de préstamos",
         "dominio": "https://colegiosarquidiocesanos.edu.co/web/",
-        "image" : SIHLogo
+        "image": SIHLogo
 
     },
     {
         "nombre": "Process Pluss",
         "descripcion": "Con Process plus, se busca gestionar la información de todo lo que tenia que ver de forma interna y externa a la entidad. Lastimosamente no tienen  páginas del producto por alguna razón aparente, así que te dejo el resultado en páginas amarillas.",
         "dominio": "https://www.paginasamarillas.com.co/empresas/process-plus-comercializadora-sas/bogota-32478502",
-        "image" : processPlusLogo
+        "image": processPlusLogo
 
     },
     {
         "nombre": "Portafolio de productos en angular",
         "descripcion": "Este portafolio es especial para mí, ya que gracias al profesor que impartió el curso, me decidí a iniciar la página web en la que te encuentras en este momento.",
         "dominio": "https://erikstor.github.io/PortafolioDeProductosAngular/",
-        "image" : angularLogo
+        "image": angularLogo
     }
 ];
 
@@ -114,7 +114,8 @@ export default function Projects() {
                     <Typography
                         gutterBottom
                     >
-                        Advertencia: el contenido de estas p&aacute;ginas puede variar ya que no son es de mi propiedad. Se
+                        Advertencia: el contenido de estas p&aacute;ginas puede variar ya que no son es de mi propiedad.
+                        Se
                         presentan solo de modo informativo.
                     </Typography>
                 </Grid>
