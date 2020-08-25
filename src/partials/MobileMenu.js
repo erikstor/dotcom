77ef8fa77b217015213862e2221ed9bbf8b30a7a
Erik Fernando Stor Franco <erik.stor.franco@gmail.com>
2020-08-24 20:29:52 -0500

    Mobile menu

--- src/partials/MobileMenu.js
+++ src/partials/MobileMenu.js
@@ -4,12 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import Home from '@material-ui/icons/Home';
+import MenuBook from '@material-ui/icons/MenuBook';
+import Computer from '@material-ui/icons/Computer';
+import EmojiObjects from '@material-ui/icons/EmojiObjects';
+
+import menuIcon from '../assest/img/menu.png';
 
 const useStyles = makeStyles({
     list: {
@@ -20,6 +25,33 @@ const useStyles = makeStyles({
     },
 });
 
+const menu = [
+    {
+        text:"INICIO",
+        icon: <Home />,
+        action: "simple-tab-0"
+    },
+    {
+        text:"HISTORIA",
+        icon: <MenuBook />,
+        action: "simple-tab-1"
+    },
+    {
+        text:"HABILIDADES",
+        icon: <Computer />,
+        action: "simple-tab-2"
+    },
+    {
+        text:"PROYECTOS",
+        icon: <EmojiObjects />,
+        action: "simple-tab-3"
+    }
+]
+
+const navegate = id => {
+    document.getElementById(id).click();
+}
+
 export default function SwipeableTemporaryDrawer() {
     const classes = useStyles();
     const [state, setState] = React.useState({
@@ -29,9 +61,7 @@ export default function SwipeableTemporaryDrawer() {
         right: false,
     });
 
-    document.querySelector('.display-mobile-menu div').addEventListener('click', function() {
 
-    }, false);
 
     const toggleDrawer = (anchor, open) => (event) => {
         if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -47,23 +77,15 @@ export default function SwipeableTemporaryDrawer() {
                 [classes.fullList]: anchor === 'top' || anchor === 'bottom',
             })}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
             onKeyDown={toggleDrawer(anchor, false)}
         >
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
+                {menu.map((current, index) => (
+                    <ListItem button key={current.action} onClick={event => {
+                        navegate(current.action)
+                    }}>
+                        <ListItemIcon>{current.icon}</ListItemIcon>
+                        <ListItemText primary={current.text} />
                     </ListItem>
                 ))}
             </List>
@@ -73,15 +95,17 @@ export default function SwipeableTemporaryDrawer() {
     return (
         <div>
             {['right'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button  onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                <React.Fragment key={anchor} >
+                    <Button id="btnMenu" onClick={toggleDrawer(anchor, true)}>
+                        <img src={menuIcon} className="size-img-menu"/>
+                    </Button>
                     <SwipeableDrawer
                         anchor={anchor}
                         open={state[anchor]}
                         onClose={toggleDrawer(anchor, false)}
                         onOpen={toggleDrawer(anchor, true)}
                     >
-                        {list(anchor)}
+                       {list(anchor)}
                     </SwipeableDrawer>
                 </React.Fragment>
             ))}
